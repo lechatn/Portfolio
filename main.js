bc360a59b2456a5d90efa15eaa9756daeda5b2bf
lechatn <noe.lechat@ynov.com>
2024-11-30 10:41:25 +0100

    final project, go to production

--- main.js
+++ main.js
@@ -5,23 +5,19 @@ function DownloadPDF() {
     link.click();
 }
 
-
-function DisplayBack(cardid){
+function DisplayBack(cardid) {
     event.preventDefault();
-    console.log("card-"+cardid);
-    document.getElementById("card-"+cardid).style.display = 'none';
-    document.getElementById("back-card-"+cardid).style.display = 'flex';
-    document.getElementById("back-card-"+cardid).style.minHeight = '300px';
-
-    
+    console.log("card-" + cardid);
+    document.getElementById("card-" + cardid).style.display = 'none';
+    document.getElementById("back-card-" + cardid).style.display = 'flex';
+    document.getElementById("back-card-" + cardid).style.minHeight = '300px';
 }
 
-function Refresh(cardid){
+function Refresh(cardid) {
     event.preventDefault();
-    document.getElementById("back-card-"+cardid).style.display = 'none';
-    document.getElementById("card-"+cardid).style.display = '';
+    document.getElementById("back-card-" + cardid).style.display = 'none';
+    document.getElementById("card-" + cardid).style.display = '';
 }
-
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contact-form');
@@ -69,46 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }, 500);
         }, 3000);
     }
-});
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    var navSkill = document.getElementById("nav-skill");
-    var skillsSection = document.getElementById("skills");
-    var navbarHeight = document.querySelector(".header").offsetHeight;
-
-    navSkill.addEventListener("click", function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: skillsSection.offsetTop - navbarHeight,
-            behavior: "smooth"
-        });
-    });
-});
-
-
-// Fonction de debounce pour limiter le nombre d'appels à adjustBackCardHeight
-function debounce(func, wait) {
-    let timeout;
-    return function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(func, wait);
-    };
-}
-
-// Désactiver les animations avant de recharger la page
-function disableAnimations() {
-    const style = document.createElement('style');
-    style.innerHTML = `
-        * {
-            transition: none !important;
-            animation: none !important;
-        }
-    `;
-    document.head.appendChild(style);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.getElementById('burger-menu');
     const navbar = document.querySelector('.navbar');
     const navLinks = document.querySelectorAll('.navbar a');
@@ -123,4 +80,15 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    const navSkill = document.getElementById("nav-skill");
+    const skillsSection = document.getElementById("skills");
+    const navbarHeight = document.querySelector(".header").offsetHeight;
+
+    navSkill.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: skillsSection.offsetTop - navbarHeight,
+            behavior: "smooth"
+        });
+    });
 });
